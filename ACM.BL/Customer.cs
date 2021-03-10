627053dd714a6a4f7069c7f09cafbf6488da1e6b
bogasa <asaftei.bogdan@gmail.com>
2021-03-10 23:55:59 +0200

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string EmailAddress { get; set; }
+        public int CustomerID { get; private set; }
+        public static int InstanceCount { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+    }
+}
---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            var customer = new Customer {FirstName = "Bogdan", LastName = "Asaftei" };
+            string expected = "Asaftei, Bogdan";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer {LastName = "Asaftei" };
+            string expected = "Asaftei";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer { FirstName = "Bogdan" };
+            string expected = "Bogdan";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            // Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Andrei";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Bogdan";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Lucian";
+            Customer.InstanceCount += 1;
+
+            // Act
+
+
+            // Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+    }
+}
